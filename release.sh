baf3fb8064266f40a98c411ab9c63fa6c7f22bad
mkristian <m.kristian@web.de>
2010-02-13 01:40:55 +0530

    release of version 0.9.0

--- release.sh
+++ release.sh
@@ -7,20 +7,20 @@ if [ "x" = "x"$2 ] ; then
 fi
 
 # first run the complete tests
-mvn clean install
+mvn3 clean install
 
 find -name "pom.xml" | xargs sed -i s/${SNAPSHOT}-SNAPSHOT/${VERSION}/
 find -name "pom.xml" | xargs git add
 git ci -m "release of version ${VERSION}" || exit
 git tag v${VERSION}
 
-mvn clean deploy -Dmaven.skip.test=true
+mvn3 clean deploy -Dmaven.skip.test=true
 
 find -name "pom.xml" | xargs sed -i s/${VERSION}/${NEXT}-SNAPSHOT/
 
 find -name "pom.xml" | xargs git add
 git ci -m "next snapshot version ${NEXT}"
 
-mvn install -Dmaven.skip.test=true
+mvn3 install -Dmaven.skip.test=true
 
 git push --tags origin master
