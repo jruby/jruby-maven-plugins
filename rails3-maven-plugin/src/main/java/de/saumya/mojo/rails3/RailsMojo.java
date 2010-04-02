57f90f060a511e613dac77e35d4989072c8bcbba
mkristian <m.kristian@web.de>
2010-04-02 07:08:51 +0530

    use rails-archetype templates to create new rails application

--- rails3-maven-plugin/src/main/java/de/saumya/mojo/rails3/RailsMojo.java
+++ rails3-maven-plugin/src/main/java/de/saumya/mojo/rails3/RailsMojo.java
@@ -1,12 +1,18 @@
 package de.saumya.mojo.rails3;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.velocity.VelocityContext;
 import org.codehaus.plexus.util.FileUtils;
+import org.codehaus.plexus.util.IOUtil;
+import org.codehaus.plexus.velocity.VelocityComponent;
 
 /**
  * goal to run rails command with the given arguments. either to generate a
@@ -14,23 +20,48 @@ import org.codehaus.plexus.util.FileUtils;
  * application.
  * 
  * @goal rails
- * @execute phase="initialize"
  */
 public class RailsMojo extends AbstractRailsMojo {
 
     /**
      * arguments for the rails command
-     * 
+     *
      * @parameter default-value="${rails.args}"
      */
-    protected String railsArgs = null;
+    protected String          railsArgs    = null;
 
     /**
      * the path to the application to be generated
-     * 
+     *
      * @parameter default-value="${app_path}"
      */
-    protected String appPath   = null;
+    protected String          appPath      = null;
+
+    /**
+     * the rails version to use
+     *
+     * @parameter default-value="3.0.0.beta" expression="${railsVersion}"
+     */
+    protected String          railsVersion = null;
+
+    /**
+     * the groupId of the new pom
+     *
+     * @parameter default-value="rails" expression="${groupId}"
+     */
+    protected String          groupId      = null;
+
+    /**
+     * the version of the new pom
+     *
+     * @parameter default-value="1.0-SNAPSHOT" expression="${version}"
+     */
+    protected String          version      = null;
+
+    /**
+     * @component
+     */
+    private VelocityComponent velocityComponent;
 
     @Override
     public void executeWithGems() throws MojoExecutionException {
@@ -52,19 +83,8 @@ public class RailsMojo extends AbstractRailsMojo {
         }
         execute(commandString, false);
         if (this.appPath != null) {
-            final File app = new File(this.project.getBasedir(), this.appPath);
-            // rectify the prolog of the script to use ruby instead of jruby
+            final File app = new File(launchDirectory(), this.appPath);
             final File script = new File(app, "script/rails");
-            try {
-                FileUtils.fileWrite(script.getAbsolutePath(),
-                                    FileUtils.fileRead(script).replace("jruby",
-                                                                       "ruby"));
-            }
-            catch (final IOException e) {
-                throw new MojoExecutionException("failed to filter " + script,
-                        e);
-            }
-
             final String database;
             final Pattern pattern = Pattern.compile(".*-d\\s+([a-z0-9]+).*");
             final Matcher matcher = pattern.matcher(commandString);
@@ -92,41 +112,65 @@ public class RailsMojo extends AbstractRailsMojo {
                 throw new MojoExecutionException("failed to filter " + script,
                         e);
             }
+            final VelocityContext context = new VelocityContext();
 
-            // write a database specific pom
-            final File pom = new File(app, "pom.xml");
-            copyFromClassloader(pom);
-            try {
-                final String pomContent = FileUtils.fileRead(pom);
-                FileUtils.fileWrite(pom.getAbsolutePath(),
-                                    pomContent.replaceFirst("__DATABASE__",
-                                                            database));
-            }
-            catch (final IOException e) {
-                throw new MojoExecutionException("failed to filter "
-                        + pom.getName(), e);
-            }
+            context.put("groupId", this.groupId);
+            context.put("artifactId", app.getName());
+            context.put("version", this.version);
+            context.put("database", database);
+            context.put("railsVersion", this.railsVersion);
+
+            filterContent(app, context, "pom.xml");
 
             // write out a new index.html
-            copyFromClassloader(new File(app, "public/index.html"));
+            filterContent(app,
+                          context,
+                          "public/maven.html",
+                          "public/index.html");
 
             // create web.xml
-            copyFromClassloader(new File(app, "src/main/webapp/WEB-INF/web.xml"));
+            filterContent(app, context, "src/main/webapp/WEB-INF/web.xml");
+
+            // create lib/tasks/jdbc.task
+            filterContent(app, context, "lib/tasks/jdbc.rake");
         }
     }
 
-    private void copyFromClassloader(final File pom)
+    private void filterContent(final File app, final VelocityContext context,
+            final String template) throws MojoExecutionException {
+        filterContent(app, context, template, template);
+    }
+
+    private void filterContent(final File app, final VelocityContext context,
+            final String template, final String targetName)
             throws MojoExecutionException {
+        final File templateFile = new File(app, targetName);
+
+        final InputStream input = getClass().getResourceAsStream("/archetype-resources/"
+                + template);
+
         try {
-            // TODO obey force, skip and pretend flags !!
-            // TODO some screen logging maybe !?\
-            FileUtils.copyURLToFile(Thread.currentThread()
-                    .getContextClassLoader()
-                    .getResource(pom.getName()), pom);
+            if (input == null) {
+                throw new FileNotFoundException(template);
+            }
+
+            final String templateString = IOUtil.toString(input);
+
+            templateFile.getParentFile().mkdirs();
+
+            final FileWriter fw = new FileWriter(templateFile);
+
+            this.velocityComponent.getEngine().evaluate(context,
+                                                        fw,
+                                                        "velocity",
+                                                        templateString);
+
+            fw.flush();
+
+            fw.close();
         }
         catch (final IOException e) {
-            throw new MojoExecutionException("error copying " + pom.getName(),
-                    e);
+            throw new MojoExecutionException("failed to filter " + template, e);
         }
     }
 }
