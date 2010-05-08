0fc2855726a6c1ac6c001a5680cb027ace333a11
mkristian <m.kristian@web.de>
2010-05-08 12:55:21 +0530

    added generation of java classes - a jruby1.5 feature

---
+++ jruby-maven-plugin/src/it/generate-java-and-compile/src/main/java/com/example/Reply.java
@@ -0,0 +1,5 @@
+package com.example;
+
+public interface Reply {
+   String reply();
+}
---
+++ jruby-maven-plugin/src/it/generate-java-and-compile/src/main/ruby/my_reply.rb
@@ -0,0 +1,10 @@
+java_import 'com.example.Reply'
+
+class MyReply
+  java_implements Reply
+
+  java_signature "String reply()"
+  def reply
+    "may all be happy"
+  end
+end
