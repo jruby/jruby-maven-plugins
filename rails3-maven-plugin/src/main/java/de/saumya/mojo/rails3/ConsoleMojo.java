cf3679a36ffae7067ab546cc698c4ce697d39f22
mkristian <m.kristian@web.de>
2010-05-08 15:50:38 +0530

    make scripts to obey gemHome and gemPath even though the scripts need to run embedded since they fire up a console

--- rails3-maven-plugin/src/main/java/de/saumya/mojo/rails3/ConsoleMojo.java
+++ rails3-maven-plugin/src/main/java/de/saumya/mojo/rails3/ConsoleMojo.java
@@ -10,22 +10,37 @@ import org.apache.maven.plugin.MojoExecutionException;
  */
 public class ConsoleMojo extends AbstractRailsMojo {
 
+    // override super mojo and make this readonly
+    /**
+     * @parameter expression="false"
+     * @readonly
+     */
+    protected boolean fork;
+
     /**
      * arguments for the console command
      * 
      * @parameter default-value="${console}"
      */
-    protected String consoleArgs = null;
+    protected String  consoleArgs = null;
 
     @Override
     protected void executeWithGems() throws MojoExecutionException {
-        String commandString = railsScript("console");
+        // make sure the whole things run in the same process
+        super.fork = false;
+        final StringBuilder commandArgs = new StringBuilder("'console'");
         if (this.args != null) {
-            commandString += " " + this.args;
+            for (final String arg : this.args.split("\\s+")) {
+                commandArgs.append(",'").append(arg).append("'");
+            }
         }
         if (this.consoleArgs != null) {
-            commandString += " " + this.consoleArgs;
+            for (final String arg : this.consoleArgs.split("\\s+")) {
+                commandArgs.append(",'").append(arg).append("'");
+            }
         }
-        execute(commandString, false);
+        execute("-e ENV['GEM_HOME']='" + this.gemHome + "';ENV['GEM_PATH']='"
+                + this.gemPath + "';ARGV<<[" + commandArgs
+                + "];ARGV.flatten!;load('" + railsScriptFile() + "');", false);
     }
 }
