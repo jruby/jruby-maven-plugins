8150f2d7a8b049138e909ae69b85f9db2015075f
mkristian <m.kristian@web.de>
2010-10-04 21:09:04 +0530

    almost working IT tests

--- rails3-maven-plugin/src/it/new_rails_sqlite3/pom2pom.rb
+++ rails3-maven-plugin/src/it/new_rails_sqlite3/pom2pom.rb
@@ -1,3 +1,8 @@
+require 'fileutils'
+Dir.glob("rails_app/*").each do |f| 
+  FileUtils.mv(f, f.sub(/rails_app./, ''))
+end
+FileUtils.rm_r("rails_app")
 pom = File.read('pom.xml').sub(/>war</,'>pom<')
 File.open('pom-base.xml', 'w') { |f| f.print(pom) }
 File.rename('pom-server.xml', 'pom.xml')
