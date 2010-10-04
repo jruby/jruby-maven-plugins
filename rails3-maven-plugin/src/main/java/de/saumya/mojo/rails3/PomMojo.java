04d7bcbc5cf3d9489e2d4106578e46f35e6e8397
mkristian <m.kristian@web.de>
2010-10-04 21:12:05 +0530

    leave some defaults of plugins to upstream script and use the plugin description for the jruby-plugins version

--- rails3-maven-plugin/src/main/java/de/saumya/mojo/rails3/PomMojo.java
+++ rails3-maven-plugin/src/main/java/de/saumya/mojo/rails3/PomMojo.java
@@ -16,63 +16,46 @@ public class PomMojo extends AbstractRailsMojo {
     /** @parameter expression="${plugin}" @readonly */
     PluginDescriptor plugin;
 
-    /** @parameter expression="${rails.pom.war}" default-value="2.1" */
-    String           warVersion;
-
-    /**
-     * @parameter expression="${rails.pom.jruby-rack}"
-     *            default-value="1.0.4.dev-SNAPSHOT"
-     */
-    String           jrubyRackVersion;
-
-    /** @parameter expression="${rails.pom.jetty}" default-value="7.1.0.RC1" */
-    String           jettyVersion;
-
     /** @parameter expression="${rails.pom.force}" default-value="false" */
     boolean          force;
+
+    /**
+     * @parameter expression="${rails.gemfile}"
+     *            default-value="${basedir}/Gemfile"
+     */
+    File             gemfile;
 
     @Override
     protected void executeRails() throws MojoExecutionException, IOException,
             ScriptException {
-        final File gemfile = new File(launchDirectory(), "Gemfile");
         final File pomfile = new File(launchDirectory(), "pom.xml");
 
-        if (gemfile.exists()) {
-            if (pomfile.exists()
-                    && gemfile.lastModified() > pomfile.lastModified()
+        if (this.gemfile.exists()) {
+            if (!(pomfile.exists() && this.gemfile.lastModified() > pomfile.lastModified())
                     || this.force) {
                 if (this.jrubyVerbose) {
                     getLog().info("create pom using following versions:");
                     getLog().info("\tjruby-plugins-version: "
                             + this.plugin.getVersion());
-                    getLog().info("\tjetty-plugin-version: "
-                            + this.jettyVersion);
-                    getLog().info("\twar-plugin-version: " + this.warVersion);
                     getLog().info("\tjruby-version: " + this.jrubyVersion);
-                    getLog().info("\tjruby-rack-version: "
-                            + this.jrubyRackVersion);
                 }
 
                 // the actual execution of the script
-                this.factory.newScriptFromResource("/maven/tools/rails_pom.rb")
-                        .addArg("{:jruby_plugins => "
-                                + this.plugin.getVersion()
-                                + ", :jruby_complete => " + this.jrubyVersion
-                                + ", :war_plugin => " + this.warVersion
-                                + ", :jruby_rack => " + this.jrubyRackVersion
-                                + ", :jetty_plugin => " + this.jettyVersion
-                                + "}")
+                this.factory.newScriptFromResource("maven/tools/rails_pom_mojo.rb")
+                        .addArg("{:jruby_complete => '" + this.jrubyVersion
+                                + "', :jruby_plugins => '"
+                                + this.plugin.getVersion() + "'}")
+                        .addArg(this.gemfile.getAbsoluteFile())
                         .executeIn(launchDirectory(), pomfile);
-
             }
             else {
                 if (this.jrubyVerbose) {
-                    getLog().info("pom is newer then Gemfile. skip creation of pom.");
+                    getLog().info("pom is newer then Gemfile. skip creation of pom. force creation with -Drails.pom.force");
                 }
             }
         }
         else {
-            getLog().warn("no Gemfile. nothing to do.");
+            getLog().warn("no Gemfile. nothing to do. please specify one with -Drails.gemfile=...");
         }
     }
 }
