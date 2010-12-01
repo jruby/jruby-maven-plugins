3144e8361a3c79f597c4d6f062486f233bc593f1
kristian <m.kristian@web.de>
2010-12-01 10:58:50 +0530

    moved integration-tests to invoker plugin

--- rspec-maven-plugin/src/it/spec-location-with-test-goal/validate.groovy
+++
@@ -1,8 +0,0 @@
-failure = false
-new File(basedir, 'build.log').eachLine{ 
-  failure = failure || (it =~ 'TOTAL: 1 passing; 0 failing; 0 pending')
-//  println "read the following line -> " + it + " " + failure
-}
-
-
-assert failure
--- rspec-maven-plugin/src/it/with-java-classes/validate.groovy
+++
@@ -1,9 +0,0 @@
-failure = false
-new File(basedir, 'build.log').eachLine{ 
-  failure = failure || (it =~ 'TOTAL: 1 passing; 0 failing; 0 pending')
-  failure = failure || (it =~ 'logging something very important')
-//  println "read the following line -> " + it + " " + failure
-}
-
-
-assert failure
