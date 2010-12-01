309510714336b0e7778f54460e639d3f619affec
kristian <m.kristian@web.de>
2010-12-01 19:11:21 +0530

    fixed missing execution of the console command

--- rails2-maven-plugin/src/main/java/de/saumya/mojo/rails/ConsoleMojo.java
+++ rails2-maven-plugin/src/main/java/de/saumya/mojo/rails/ConsoleMojo.java
@@ -3,6 +3,7 @@ package de.saumya.mojo.rails;
 import java.io.IOException;
 
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
 
 import de.saumya.mojo.ruby.script.ScriptException;
 
@@ -23,13 +24,14 @@ public class ConsoleMojo extends AbstractRailsMojo {
     protected String consoleArgs = null;
 
     @Override
-    public void execute() throws MojoExecutionException {
+    public void execute() throws MojoExecutionException, MojoFailureException {
         if (this.jrubyVersion != null
                 && this.jrubyVersion.compareTo("1.5.0") < 0) {
             throw new MojoExecutionException("does not work with jruby version < 1.5.0");
         }
         // make sure the whole things run in the same process
         this.jrubyFork = false;
+        super.execute();
     }
 
     @Override
