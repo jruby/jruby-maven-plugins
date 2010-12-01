d2a32d238b46f3f6ddd2b63d043f78a14317dab3
kristian <m.kristian@web.de>
2010-12-01 19:14:09 +0530

    more cleanup for integration tests

--- rails3-maven-plugin/src/it/new_rails_postgres/pom2pom.rb
+++ rails3-maven-plugin/src/it/new_rails_postgres/pom2pom.rb
@@ -5,6 +5,7 @@ Dir.glob("rails_app/*").each do |f|
   FileUtils.mv(f, path)
 end
 FileUtils.rm_r("rails_app")
+FileUtils.rm_r(File.join("target", "jetty"))
 pom = File.read('pom.xml').sub(/>war</,'>pom<')
 File.open('pom-base.xml', 'w') { |f| f.print(pom) }
 File.rename('pom-server.xml', 'pom.xml')
