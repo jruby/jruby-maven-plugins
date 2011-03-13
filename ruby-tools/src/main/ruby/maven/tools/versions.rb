14020aace70983ddf38db38b01b8a75d15d74496
kristian <m.kristian@web.de>
2011-03-14 01:29:34 +0530

    prepare release

--- ruby-tools/src/main/ruby/maven/tools/versions.rb
+++ ruby-tools/src/main/ruby/maven/tools/versions.rb
@@ -4,7 +4,7 @@
       :jetty_plugin => "7.2.2.v20101205", #7.1.0.RC1",
       :jruby_rack => "1.0.7",
       :war_plugin => "2.1.1",
-      :jruby_plugins => "0.25.0-SNAPSHOT",
+      :jruby_plugins => "0.25.0",
       :jruby_version => "1.5.6",
     }.freeze
   end
