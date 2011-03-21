895308854715e3f460e419cde6329691384007c4
kristian <m.kristian@web.de>
2011-03-22 00:44:10 +0530

    added primitive bundler support

---
+++ bundler-maven-plugin/src/main/java/de/saumya/mojo/bundler/InstallMojo.java
@@ -0,0 +1,71 @@
+package de.saumya.mojo.cucumber;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.sonatype.aether.RepositorySystemSession;
+
+import de.saumya.mojo.gem.AbstractGemMojo;
+import de.saumya.mojo.ruby.gems.GemException;
+import de.saumya.mojo.ruby.script.Script;
+import de.saumya.mojo.ruby.script.ScriptException;
+
+/**
+ * maven wrapper around the bundler command.
+ * 
+ * @goal install
+ * @phase initialize
+ * @requiresDependencyResolution test
+ */
+public class InstallMojo extends AbstractGemMojo {
+
+    /**
+     * arguments for the bundler command.
+     * 
+     * @parameter default-value="${bundler.args}"
+     */
+    private final String bundlerArgs = null;
+
+    /**
+     * bundler version used when there is no pom. defaults to latest version.
+     * 
+     * @parameter default-value="${bundler.version}"
+     */
+    private final String bundlerVersion = null;
+
+    /**
+     * @parameter default-value="${repositorySystemSession}"
+     * @readonly
+     */
+    private RepositorySystemSession repoSession;
+
+    @Override
+    public void executeWithGems() throws MojoExecutionException, 
+					 ScriptException, 
+					 IOException, 
+					 GemException {
+	final Script script = this.factory.newScriptFromSearchPath("bundle");
+	script.addArg("install");
+	if (this.project.getBasedir() == null) {
+
+	    this.gemsInstaller.installGem("bundler", 
+					  this.bundlerVersion,
+					  this.repoSession, 
+					  this.localRepository);
+
+	}
+	else {
+	    script.addArg("--quiet");
+	}
+	if (this.bundlerArgs != null) {
+	    script.addArgs(this.bundlerArgs);
+	}
+	if (this.args != null) {
+	    script.addArgs(this.args);
+	}
+
+	script.executeIn(launchDirectory());
+    }
+}
