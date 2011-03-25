ea5bedfeb6df3f6bcb5c8b15f06986589ffbeacf
kristian <m.kristian@web.de>
2011-03-25 19:52:38 +0530

    removed obsolete

--- ruby-tools/src/main/ruby/maven/tools/gemfile_execute_phase.rb
+++
@@ -1,7 +0,0 @@
-require File.join(File.dirname(__FILE__), 'gemfile_reader.rb')
-
-warn "deprecated, use it with 'Mavenfile' instead"
-
-gemfile = Maven::Tools::GemfileReader.new(File.new(ARGV[0]))
-gemfile.execute_phase(ARGV[1])
-
--- ruby-tools/src/main/ruby/maven/tools/rails_pom_mojo.rb
+++
@@ -1,8 +0,0 @@
-require File.join(File.dirname(__FILE__), 'rails_pom.rb')
-
-warn "deprecated, use rails_project instead"
-
-# TODO usage
-maven = Maven::Tools::RailsPom.new(eval(ARGV[0]))
-pom = maven.create_pom(File.new(ARGV[1]))
-puts pom.to_xml
