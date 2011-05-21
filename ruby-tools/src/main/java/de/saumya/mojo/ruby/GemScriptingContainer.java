6e0834e5ddc07ba8f4819460506f1b913c587d30
Kristian Meier <m.kristian@web.de>
2011-05-21 21:48:16 +0530

    added more constructors + reformating :-(

--- ruby-tools/src/main/java/de/saumya/mojo/ruby/GemScriptingContainer.java
+++ ruby-tools/src/main/java/de/saumya/mojo/ruby/GemScriptingContainer.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package de.saumya.mojo.ruby;
 
@@ -15,47 +15,65 @@ import org.jruby.embed.ScriptingContainer;
 
 public class GemScriptingContainer extends ScriptingContainer {
 
-	public GemScriptingContainer(final File gemHome, final File gemPath) {
-		super(LocalContextScope.SINGLETON, LocalVariableBehavior.PERSISTENT);
-		final Map<String, String> env = new HashMap<String, String>();
-		if (gemHome != null && gemHome.exists()) {
-			env.put("GEM_HOME", gemHome.getAbsolutePath());
-		}
-		if (gemPath != null && gemPath.exists()) {
-			env.put("GEM_PATH", gemPath.getAbsolutePath());
-		}
-		getProvider().getRubyInstanceConfig().setEnvironment(env);
+    public GemScriptingContainer(final File gemHome, final File gemPath) {
+        this(LocalContextScope.SINGLETON,
+                LocalVariableBehavior.PERSISTENT,
+                gemHome,
+                gemPath);
+    }
 
-		// setting the JRUBY_HOME to the one from the jruby jar - ignoring
-		// the environment setting !
-		getProvider().getRubyInstanceConfig().setJRubyHome(
-				Thread.currentThread().getContextClassLoader()
-						.getResource("META-INF/jruby.home").toString()
-						.replaceFirst("^jar:", ""));
-	}
+    public GemScriptingContainer(LocalContextScope scope,
+            LocalVariableBehavior behavior) {
+        this(scope, behavior, null, null);
+    }
 
-	public Object runScriptletFromClassloader(final String name,
-			final Class<?> clazz) throws IOException {
-		InputStream script = ScriptUtils.getScriptAsStream(name, clazz);
-		try {
-			return runScriptlet(script, name);
-		} finally {
-			if (script != null) {
-				script.close();
-			}
-		}
-	}
+    public GemScriptingContainer(LocalContextScope scope,
+            LocalVariableBehavior behavior, final File gemHome,
+            final File gemPath) {
+        super(scope, behavior);
+        final Map<String, String> env = new HashMap<String, String>();
+        if (gemHome != null && gemHome.exists()) {
+            env.put("GEM_HOME", gemHome.getAbsolutePath());
+        }
+        if (gemPath != null && gemPath.exists()) {
+            env.put("GEM_PATH", gemPath.getAbsolutePath());
+        }
+        getProvider().getRubyInstanceConfig().setEnvironment(env);
 
-	public Object runScriptletFromClassloader(final String name)
-			throws IOException {
-		InputStream script = ScriptUtils.getScriptAsStream(name);
-		try {
-			return runScriptlet(ScriptUtils.getScriptAsStream(name), name);
-		} finally {
-			if (script != null) {
-				script.close();
-			}
-		}
-	}
+        // setting the JRUBY_HOME to the one from the jruby jar - ignoring
+        // the environment setting !
+        getProvider().getRubyInstanceConfig()
+                .setJRubyHome(Thread.currentThread()
+                        .getContextClassLoader()
+                        .getResource("META-INF/jruby.home")
+                        .toString()
+                        .replaceFirst("^jar:", ""));
+    }
+
+    public Object runScriptletFromClassloader(final String name,
+            final Class<?> clazz) throws IOException {
+        InputStream script = ScriptUtils.getScriptAsStream(name, clazz);
+        try {
+            return runScriptlet(script, name);
+        }
+        finally {
+            if (script != null) {
+                script.close();
+            }
+        }
+    }
+
+    public Object runScriptletFromClassloader(final String name)
+            throws IOException {
+        InputStream script = ScriptUtils.getScriptAsStream(name);
+        try {
+            return runScriptlet(ScriptUtils.getScriptAsStream(name), name);
+        }
+        finally {
+            if (script != null) {
+                script.close();
+            }
+        }
+    }
 
 }
