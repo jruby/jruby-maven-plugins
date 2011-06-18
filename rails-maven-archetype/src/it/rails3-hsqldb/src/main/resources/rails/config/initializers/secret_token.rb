402995e4333c4e67cb26f11e9c40adae3eaafd40
Kristian Meier <m.kristian@web.de>
2011-06-18 11:03:21 +0530

    no further: focus on rails3 and its Gemfile, old archetypes are good enough for rails2

--- rails-maven-archetype/src/it/rails3-hsqldb/src/main/resources/rails/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-Rails.application.config.secret_token = 'cdc9b6e3147544e63e4f5ecb26b92c06aff1be9f06438bc9db4d584bba2f1d771ab2c539d4737f20dc08fe8ef739ebdbd24d175387c7325f58dffd7a5f96e3ba'
