8c8f895b003075d8ebc609397eb07b93154a677e
Kristian Meier <m.kristian@web.de>
2011-06-18 11:12:05 +0530

    new default versions

--- ruby-tools/src/main/ruby/maven/tools/versions.rb
+++ ruby-tools/src/main/ruby/maven/tools/versions.rb
@@ -1,8 +1,8 @@
  module Maven
   module Tools
     VERSIONS = { 
-      :jetty_plugin => "7.2.2.v20101205",
-      :jruby_rack => "1.0.7",
+      :jetty_plugin => "7.4.2.v20110526",
+      :jruby_rack => "1.0.9",
       :war_plugin => "2.1.1",
       :jruby_plugins => "@project.version@",
       :jruby_version => "@jruby.version@",
