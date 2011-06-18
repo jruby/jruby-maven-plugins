1fd3b90d9b18f4e570bffb268c1ab5e3644d3e87
Kristian Meier <m.kristian@web.de>
2011-06-18 10:55:56 +0530

    skip bundle install if pom is unchanged

--- bundler-maven-plugin/src/main/java/de/saumya/mojo/bundler/InstallMojo.java
+++ bundler-maven-plugin/src/main/java/de/saumya/mojo/bundler/InstallMojo.java
@@ -1,8 +1,10 @@
 package de.saumya.mojo.bundler;
 
+import java.io.File;
 import java.io.IOException;
 
 import org.apache.maven.plugin.MojoExecutionException;
+import org.codehaus.plexus.util.FileUtils;
 import org.sonatype.aether.RepositorySystemSession;
 
 import de.saumya.mojo.gem.AbstractGemMojo;
@@ -11,7 +13,7 @@ import de.saumya.mojo.ruby.script.Script;
 import de.saumya.mojo.ruby.script.ScriptException;
 
 /**
- * maven wrapper around the bundler command.
+ * maven wrapper around the bundler install command.
  * 
  * @goal install
  * @phase initialize
@@ -24,14 +26,14 @@ public class InstallMojo extends AbstractGemMojo {
      * 
      * @parameter default-value="${bundler.args}"
      */
-    private final String bundlerArgs = null;
+    private final String            bundlerArgs    = null;
 
     /**
      * bundler version used when there is no pom. defaults to latest version.
      * 
      * @parameter default-value="${bundler.version}"
      */
-    private final String bundlerVersion = null;
+    private final String            bundlerVersion = null;
 
     /**
      * @parameter default-value="${repositorySystemSession}"
@@ -40,31 +42,39 @@ public class InstallMojo extends AbstractGemMojo {
     private RepositorySystemSession repoSession;
 
     @Override
-    public void executeWithGems() throws MojoExecutionException, 
-					 ScriptException, 
-					 IOException, 
-					 GemException {
-	final Script script = this.factory.newScriptFromSearchPath("bundle");
-	script.addArg("install");
-	if (this.project.getBasedir() == null) {
+    public void executeWithGems() throws MojoExecutionException,
+            ScriptException, IOException, GemException {
+        if(project.getFile() != null){
+            File bundlerTouch = new File(project.getBuild().getDirectory(), "bundler-lastrun-timestamp");
+            if (bundlerTouch.exists() && bundlerTouch.lastModified() > project.getFile().lastModified()) {
+                getLog().debug("skip bundler install since pom did not change since last run");
+                return;
+            }
+            else {
+                FileUtils.fileWrite(bundlerTouch.getAbsolutePath(), "");
+            }
+        }
+        final Script script = this.factory.newScriptFromSearchPath("bundle");
+        script.addArg("install");
+        if (this.project.getBasedir() == null) {
 
-	    this.gemsInstaller.installGem("bundler", 
-					  this.bundlerVersion,
-					  this.repoSession, 
-					  this.localRepository);
+            this.gemsInstaller.installGem("bundler",
+                                          this.bundlerVersion,
+                                          this.repoSession,
+                                          this.localRepository);
 
-	}
-	else {
-	    script.addArg("--quiet");
-	    script.addArg("--local");
-	}
-	if (this.bundlerArgs != null) {
-	    script.addArgs(this.bundlerArgs);
-	}
-	if (this.args != null) {
-	    script.addArgs(this.args);
-	}
+        }
+        else {
+            script.addArg("--quiet");
+            script.addArg("--local");
+        }
+        if (this.bundlerArgs != null) {
+            script.addArgs(this.bundlerArgs);
+        }
+        if (this.args != null) {
+            script.addArgs(this.args);
+        }
 
-	script.executeIn(launchDirectory());
+        script.executeIn(launchDirectory());
     }
 }
