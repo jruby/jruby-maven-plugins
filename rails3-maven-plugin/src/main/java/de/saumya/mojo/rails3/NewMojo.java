6a824095b04e2b5a579b885f20ab2098cd6afbdf
Kristian Meier <m.kristian@web.de>
2011-06-18 11:21:26 +0530

    added orm switch and remove rails-3.0.x restriction

--- rails3-maven-plugin/src/main/java/de/saumya/mojo/rails3/NewMojo.java
+++ rails3-maven-plugin/src/main/java/de/saumya/mojo/rails3/NewMojo.java
@@ -10,57 +10,89 @@ import org.apache.maven.plugin.MojoFailureException;
 
 import de.saumya.mojo.ruby.gems.GemException;
 import de.saumya.mojo.ruby.rails.RailsException;
+import de.saumya.mojo.ruby.rails.RailsManager.ORM;
 import de.saumya.mojo.ruby.script.ScriptException;
 
 /**
  * goal to run rails command with the given arguments. either to generate a
  * fresh rails application or to run the rails script from within a rails
  * application.
- * 
+ *
  * @goal new
  */
 public class NewMojo extends AbstractRailsMojo {
+
     /**
      * arguments for the rails command
-     * 
+     * <br/>
+     * Command line -Drails.args=...
+     *
      * @parameter default-value="${rails.args}"
      */
     protected String            railsArgs                      = null;
 
     /**
      * the path to the application to be generated
-     * 
+     * <br/>
+     * Command line -Dapp_path=...
+     *
      * @parameter default-value="${app_path}"
      */
     protected File              appPath                        = null;
 
     /**
      * the database to use. DEFAULT: sqlite3
-     * 
+     * <br/>
+     * Command line -Ddatabase=...
+     *
      * @parameter default-value="${database}"
      */
     protected String            database                       = null;
 
     /**
      * the rails version to use
-     * 
+     * <br/>
+     * Command line -Drails.version=...
+     *
      * @parameter expression="${rails.version}"
      */
     protected String            railsVersion                   = null;
 
     /**
      * the groupId of the new pom
-     * 
+     * <br/>
+     * Command line -DgroupId=...
+     *
      * @parameter default-value="rails" expression="${groupId}"
      */
     protected String            groupId                        = null;
 
     /**
      * the version of the new pom
-     * 
+     * <br/>
+     * Command line -Dversion=...
+     *
      * @parameter default-value="1.0-SNAPSHOT" expression="${version}"
      */
     protected String            artifactVersion                = null;
+
+//    /**
+//     * use latest prerelease version unless a rails version is specified.
+//     * <br/>
+//     * Command line -Dprerelease=...
+//     *
+//     * @parameter expression="${prerelease}" default-value="false"
+//     */
+//    protected boolean usePrerelease;
+
+    /**
+     * use latest prerelease version unless a rails version is specified.
+     * <br/>
+     * Command line -Dorm=activerecord or -Dorm=datamapper
+     *
+     * @parameter expression="${rails.orm}" default-value="activerecord"
+     */
+    protected String railsORM;
 
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
@@ -72,8 +104,8 @@ public class NewMojo extends AbstractRailsMojo {
     @Override
     void executeRails() throws MojoExecutionException, ScriptException,
             IOException, GemException, RailsException {
-        if(railsVersion != null && !this.railsVersion.startsWith("3.0.")) {
-            throw new MojoExecutionException("given rails version is not rails-3.0.x : "
+        if(railsVersion != null && !this.railsVersion.startsWith("3.")) {
+            throw new MojoExecutionException("given rails version is not rails-3.x.y : "
                                              + this.railsVersion);
         }
         try {
@@ -113,6 +145,7 @@ public class NewMojo extends AbstractRailsMojo {
                                         this.appPath,
                                         this.database,
                                         this.railsVersion,
+                                        ORM.valueOf(this.railsORM),
                                         combArgs);
         }
         catch (final RailsException e) {
