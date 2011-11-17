b17a077f822f40c3b83bc12b6ab2d52207f49c30
Kristian Meier <m.kristian@web.de>
2011-11-17 08:53:55 +0530

    extra template goes in the end !

--- ruby-tools/src/main/resources/rails-templates/templates.rb
+++ ruby-tools/src/main/resources/rails-templates/templates.rb
@@ -2,10 +2,11 @@ base = java.lang.System.getProperty('maven.rails.basetemplate')
 extra = java.lang.System.getProperty('maven.rails.extratemplate')
 
 apply "#{base}" if base
-apply "#{extra}" if extra
 
 if gwt = java.lang.System.getProperty('maven.rails.gwt')
   
   gem 'resty-generators'
 
 end
+
+apply "#{extra}" if extra
