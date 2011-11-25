393f83e6ddb8e03cba406103561ae2b9ec3d1dac
Kristian Meier <m.kristian@web.de>
2011-11-25 14:39:03 +0530

    explicit jruby version

--- ruby-tools/src/main/ruby/maven/tools/versions.rb
+++ ruby-tools/src/main/ruby/maven/tools/versions.rb
@@ -5,7 +5,7 @@
       :jruby_rack => "@jruby.rack.version@",
       :war_plugin => "@war.version@",
       :jruby_plugins => "@project.version@",
-      :jruby_version => "@jruby.version@",
+      :jruby_version => defined?(JRUBY_VERSION) ? JRUBY_VERSION : '1.6.5'
     }.freeze
   end
 end
