567e7a79e65275b082cdfa70f227982261d94f4e
Kristian Meier <m.kristian@web.de>
2012-04-16 23:37:48 +0530

    latest jruby version

--- ruby-tools/src/main/ruby/maven/tools/versions.rb
+++ ruby-tools/src/main/ruby/maven/tools/versions.rb
@@ -4,8 +4,9 @@
       :jetty_plugin => "@jetty.version@",
       :jruby_rack => "@jruby.rack.version@",
       :war_plugin => "@war.version@",
+      :jar_plugin => "@jar.version@",
       :jruby_plugins => "@project.version@",
-      :jruby_version => defined?(JRUBY_VERSION) ? JRUBY_VERSION : '1.6.5'
+      :jruby_version => defined?(JRUBY_VERSION) ? JRUBY_VERSION : '1.6.7'
     }.freeze
   end
 end
