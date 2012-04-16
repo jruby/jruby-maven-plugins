6a64f0a1c790d4af399bf634b694e8caa95f5ac2
Kristian Meier <m.kristian@web.de>
2012-04-17 01:17:13 +0530

    uses common test result parser

--- runit-maven-plugin/src/main/java/de/saumya/mojo/runit/RUnitMojo.java
+++ runit-maven-plugin/src/main/java/de/saumya/mojo/runit/RUnitMojo.java
@@ -2,11 +2,9 @@ package de.saumya.mojo.runit;
 
 import java.io.File;
 import java.io.IOException;
-import java.text.MessageFormat;
 
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
-import org.codehaus.plexus.util.FileUtils;
 
 import de.saumya.mojo.ruby.script.Script;
 import de.saumya.mojo.ruby.script.ScriptException;
@@ -50,20 +48,24 @@ public class RUnitMojo extends AbstractTestMojo {
      */
     protected boolean skipRunit;
 
+    private TestResultManager resultManager;
+    private File outputfile;
+    
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
         if (this.skipTests || this.skipRunit) {
             getLog().info("Skipping RUnit tests");
             return;
         } else {
+            outputfile = new File(this.project.getBuild().getDirectory()
+                    .replace("${project.basedir}/", ""), "runit.txt");
+            resultManager = new TestResultManager(project.getName(), "runit", testReportDirectory, summaryReport);
             super.execute();
         }
     }
 
     protected Result runIt(ScriptFactory factory, Mode mode, String version, TestScriptFactory scriptFactory)
             throws IOException, ScriptException, MojoExecutionException {
-        final File outputfile = new File(this.project.getBuild().getDirectory()
-                .replace("${project.basedir}/", ""), "runit.txt");
 
         scriptFactory.setOutputDir(outputfile.getParentFile());
         scriptFactory.setReportPath(outputfile);
@@ -73,8 +75,6 @@ public class RUnitMojo extends AbstractTestMojo {
         else{
             scriptFactory.setSourceDir(new File(launchDirectory(), runitDirectory));
         }
-
-        scriptFactory.emit();
 
         final Script script = factory.newScript(scriptFactory.getCoreScript());
         if (this.runitArgs != null) {
@@ -90,52 +90,7 @@ public class RUnitMojo extends AbstractTestMojo {
             getLog().debug("exception in running tests", e);
         }
 
-        Result result = new Result();
-        String time = null;
-        for (Object lineObj : FileUtils.loadFile(outputfile)) {
-            String line = lineObj.toString();
-            if (line.contains("Finished")) {
-                time = line.replaceAll("[a-zA-Z]+\\.?", "").trim();
-            }
-            if (line.contains("failures")) {
-                result.message = line;
-                int[] vector = new int[5];
-                int i = 0;
-                String statusLine = line.replaceAll("[a-z]+,?", "");
-                for (String n : statusLine.split("\\s+")) {
-                    vector[i++] = Integer.parseInt(n);
-                }
-                result.success = (vector[ResultEnum.FAILURES.ordinal()] == 0)
-                        && (vector[ResultEnum.ERRORS.ordinal()] == 0);
-
-                // TODO should be a real report with testcases
-                String surefireXml = MessageFormat
-                        .format(
-                                "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n"
-                                        + "<testsuite time=\"{0}\" errors=\"{1}\" tests=\"{2}\" skipped=\"{3}\" failures=\"{4}\" name=\"{5}\">\n"
-                                        + "</testsuite>\n", time,
-                                vector[ResultEnum.ERRORS.ordinal()],
-                                vector[ResultEnum.TESTS.ordinal()],
-                                vector[ResultEnum.SKIPS.ordinal()],
-                                vector[ResultEnum.FAILURES.ordinal()], project
-                                        .getName());
-
-                testReportDirectory.mkdirs();
-                String filename = "TEST-runit"
-                        + (mode.flag == null ? "" : "-" + version + mode.flag)
-                        + ".xml";
-                FileUtils.fileWrite(new File(testReportDirectory, filename)
-                        .getAbsolutePath(), "UTF-8", surefireXml);
-                if (summaryReport != null) {
-                    FileUtils.fileWrite(summaryReport.getAbsolutePath(),
-                            "UTF-8", surefireXml);
-                }
-                return result;
-            }
-        }
-        result.message = "did not find test summary";
-        result.success = false;
-        return result;
+        return resultManager.generateReports(mode, version, outputfile);
     }
 
     @Override
