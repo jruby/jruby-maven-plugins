8587c6e986f25ef9a80e8579225d57b5fbe8acf9
Kristian Meier <m.kristian@web.de>
2012-05-10 19:03:24 +0530

    clean up

--- rspec-maven-plugin/src/main/java/de/saumya/mojo/rspec/RSpecMojo.java
+++ rspec-maven-plugin/src/main/java/de/saumya/mojo/rspec/RSpecMojo.java
@@ -28,27 +28,6 @@ import de.saumya.mojo.tests.TestScriptFactory;
  */
 public class RSpecMojo extends AbstractTestMojo {
 
-//    /**
-//     * The project base directory
-//     * 
-//     * @parameter expression="${basedir}"
-//     * @required
-//     * @readonly
-//     */
-//    protected File                  basedir;
-//
-//    /**
-//     * The classpath elements of the project being tested.
-//     * 
-//     * @parameter expression="${project.testClasspathElements}"
-//     * @required
-//     * @readonly
-//     */
-//    protected List<String>          classpathElements;
-//
-//    /** @parameter default-value="${skipSpecs}" */
-//    protected boolean               skipSpecs = false;
-
     /**
      * arguments for the rspec command. <br/>
      * Command line -Drspec.args=...
@@ -74,14 +53,6 @@ public class RSpecMojo extends AbstractTestMojo {
      */
     protected boolean skipSpecs;
 
-//    /**
-//     * The directory where the RSpec report will be written to
-//     * 
-//     * @parameter expression="target"
-//     * @required
-//     */
-//    protected File                  outputDirectory;
-//
     /**
      * The name of the RSpec report.
      * 
@@ -89,37 +60,8 @@ public class RSpecMojo extends AbstractTestMojo {
      */
     private String                reportName;
 
-//    /**
-//     * List of system properties to set for the tests.
-//     * 
-//     * @parameter
-//     */
-//    protected Properties            systemProperties;
-
-//    /**
-//     * rspec version used when there is no pom. default is latest version.
-//     * 
-//     * @parameter default-value="${rspec.version}"
-//     */
-//    private String                  rspecVersion;
-
-//    /**
-//     * @parameter default-value="${repositorySystemSession}"
-//     * @readonly
-//     */
-//    private RepositorySystemSession repoSession;
-//
-//    private ScriptFactory           rspecScriptFactory;
-
-//    private String                  reportPath;
-
-  //  private TestResultManager resultManager;
     private File outputfile;
     
-//    private File specSourceDirectory() {
-//        return new File(launchDirectory(), this.specSourceDirectory);
-//    }
-
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
         if (this.skipTests || this.skipSpecs) {
@@ -129,46 +71,9 @@ public class RSpecMojo extends AbstractTestMojo {
         else {
             outputfile = new File(this.project.getBuild().getDirectory()
                                   .replace("${project.basedir}/", ""), reportName);
-//            resultManager = new TestResultManager(project.getName(), "minispec", testReportDirectory, summaryReport);
-//
             super.execute();
         }
     }
-
-//    @Override
-//    public void executeWithGemss() throws MojoExecutionException,
-//            ScriptException, IOException, GemException {
-//        final File specSourceDirectory = specSourceDirectory();
-//        if (!specSourceDirectory.exists() && this.args == null) {
-//            getLog().info("Skipping RSpec tests since " + specSourceDirectory
-//                    + " is missing");
-//            return;
-//        }
-//        getLog().info("Running RSpec tests from " + specSourceDirectory);
-//
-//        if (this.project.getBasedir() == null) {
-//
-//            this.rspecVersion = this.gemsInstaller.installGem("rspec",
-//                                                              this.rspecVersion,
-//                                                              this.repoSession,
-//                                                              this.localRepository)
-//                    .getVersion();
-//
-//        }
-//
-//        this.reportPath = new File(this.outputDirectory, this.reportName).getAbsolutePath();
-//
-//        initScriptFactory(getRSpecScriptFactory(), this.reportPath);
-//
-//        try {
-//            this.rspecScriptFactory.emit();
-//        }
-//        catch (final Exception e) {
-//            getLog().error("error emitting .rb", e);
-//        }
-//
-//        super.executeWithGems();
-//    }
     
     protected Result runIt(de.saumya.mojo.ruby.script.ScriptFactory factory, Mode mode, String version, TestScriptFactory scriptFactory)
             throws IOException, ScriptException, MojoExecutionException {
@@ -265,148 +170,9 @@ public class RSpecMojo extends AbstractTestMojo {
       }
       return result;
     }
-//    protected Result runIt(de.saumya.mojo.ruby.script.ScriptFactory factory,
-//            Mode mode, String version) throws IOException, ScriptException,
-//            MojoExecutionException {
-//        
-//        Script script = factory.newScript(this.rspecScriptFactory.getScriptFile());
-//        script.addArgs(this.args);
-//        script.executeIn(launchDirectory());
-//
-//        final File reportFile;
-//        if (mode != Mode.DEFAULT) {
-//            reportFile = new File(reportPath.replace(".html", "-" + version
-//                    + mode.name() + ".html"));
-//        }
-//        else if (this.jrubyVersion.equals(version)) {
-//            reportFile = new File(reportPath);
-//        }
-//        else {
-//            reportFile = new File(reportPath.replace(".html", "-" + version
-//                    + ".html"));
-//        }
-//        new File(reportPath).renameTo(reportFile);
-//
-//        Result result = new Result();
-//        Reader in = null;
-//        try {
-//            in = new FileReader(reportFile);
-//            final BufferedReader reader = new BufferedReader(in);
-//
-//            String line = null;
-//
-//            while ((line = reader.readLine()) != null) {
-//                // singular case needs to be treated as well
-//                if (line.contains("failure") && line.contains("example")) {
-//                    result.message = line.replaceFirst("\";</.*>", "")
-//                            .replaceFirst("<.*\"", "");
-//                    break;
-//                }
-//            }
-//        }
-//        catch (final IOException e) {
-//            throw new MojoExecutionException("Unable to read test report file: "
-//                    + reportFile);
-//        }
-//        finally {
-//            if (in != null) {
-//                try {
-//                    in.close();
-//                }
-//                catch (final IOException e) {
-//                    throw new MojoExecutionException(e.getMessage());
-//                }
-//            }
-//        }
-//
-//		if (result.message == null) {
-//		    if(reportFile.length() == 0){
-//	            result.success = true;
-//		    }
-//		    else { // this means the report file partial and thus an error occured
-//		        result.message = "An unknown error occurred";
-//		        result.success = false;
-//		    }
-//		}
-//        else {
-//			result.success = result.message.contains("0 failures");
-//        }
-//        return result;
-//    }
 
     @Override
     protected TestScriptFactory newTestScriptFactory(Mode mode) {
         return new RSpecMavenTestScriptFactory();
     }
-
-//    private void initScriptFactory(final ScriptFactory factory,
-//            final String reportPath) {
-//        factory.setBaseDir(this.basedir.getAbsolutePath());
-//        factory.setSummaryReport(this.summaryReport);
-//        factory.setOutputDir(this.outputDirectory);
-//        factory.setReportPath(reportPath);
-//        factory.setSourceDir(specSourceDirectory().getAbsolutePath());
-//        factory.setClasspathElements(this.classpathElements);
-//        factory.setGemHome(this.gemHome);
-//        factory.setGemPaths(new File[] {
-//                this.gemPath,
-//                new File(this.gemPath.getParentFile(), this.gemPath.getName()
-//                        + "-rspec-maven-plugin") });
-//        Properties props = this.systemProperties;
-//        if (props == null) {
-//            props = new Properties();
-//        }
-//        factory.setSystemProperties(props);
-//    }
-
-//    private ScriptFactory scriptFactory4Versionn(String version) {
-//        if (version.startsWith("1.")) {
-//            return new RSpec1ScriptFactory();
-//        }
-//        else if (version.startsWith("2.")) {
-//            return new RSpec2ScriptFactory();
-//        }
-//        else {
-//            return null;
-//        }
-//    }
-//
-//    private ScriptFactory getRSpecScriptFactory() throws MojoExecutionException {
-//        if (this.rspecScriptFactory != null) {
-//            return this.rspecScriptFactory;
-//        }
-//
-//        this.rspecScriptFactory = getRSpecScriptFactory(project.getDependencyArtifacts());
-//        if (this.rspecScriptFactory == null) {
-//            this.rspecScriptFactory = getRSpecScriptFactory(plugin.getArtifacts());
-//        }
-//
-//        // get the script-factory when there is no pom
-//        if (this.rspecScriptFactory == null) {
-//            if(this.rspecVersion == null && this.project.getBasedir() != null){
-//                throw new MojoExecutionException("please add a gem dependency for rspec to your POM");
-//            }
-//            this.rspecScriptFactory = new RSpec2ScriptFactory();//scriptFactory4Version(this.rspecVersion);
-//        }
-//
-//        if (this.rspecScriptFactory == null) {
-//            throw new MojoExecutionException("Unable to determine version of RSpec");
-//        }
-//
-//        return this.rspecScriptFactory;
-//    }
-//
-//    private ScriptFactory getRSpecScriptFactory(
-//            Collection<Artifact> dependencyArtifacts) {
-//        for (Artifact each : dependencyArtifacts) {
-//            // allow all scope, since with deps within plugins the scope is less
-//            // important
-//            if (each.getGroupId().equals("rubygems")
-//                    && each.getArtifactId().equals("rspec")) {
-//                return scriptFactory4Version(each.getVersion());
-//            }
-//        }
-//        return null;
-//    }
-
 }
