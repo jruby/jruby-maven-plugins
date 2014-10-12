291a2a11be433058e8f084bf478681dc8282c1dd
Christian Meier <m.kristian@web.de>
2014-10-13 00:32:57 +0100

    share installed gems over ITs

--- ruby-tools/src/main/scripts/install_gems.rb
+++ ruby-tools/src/main/scripts/install_gems.rb
@@ -4,10 +4,16 @@ require 'fileutils'
 Dir[ 'target/dependency/*gem' ].each do |file|
   installer = Gem::Installer.new( file,
                                   :ignore_dependencies => true,
-                                  :install_dir => 'target/rubygems' )
+                                  :install_dir => '../target/rubygems' )
   installer.install
 end
 
-Dir[ 'target/rubygems/gems/*/lib/*' ].each do |f|
-  FileUtils.cp_r( "#{f}", 'target/classes' )
+Dir[ 'target/dependency/*gem' ].each do |file|
+  f = "../target/rubygems/gems/#{File.basename(file).sub(/.gem/, '')}/lib"
+  unless File.exists?( f )
+    f = "../target/rubygems/gems/#{File.basename(file).sub(/.gem/, '')}-java/lib"
+  end
+  Dir[ File.expand_path( f ) + "/*" ].each do |ff|
+    FileUtils.cp_r( "#{ff}", 'target/classes' )
+  end
 end
