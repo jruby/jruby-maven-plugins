db4ad4fbff641868087cc008908d7216872001cb
Christian Meier <m.kristian@web.de>
2015-07-13 15:07:16 +0200

    fixes war ITs

--- jruby9/jruby9-extensions/src/it/jrubyWarExample/spec/one_spec.rb
+++ jruby9/jruby9-extensions/src/it/jrubyWarExample/spec/one_spec.rb
@@ -2,9 +2,9 @@ require 'jar-dependencies'
 describe 'something' do
 
   it 'runs inside the right environment' do
-    expect(Dir.pwd).to eq 'uri:classloader://'
-    expect(__FILE__).to eq 'uri:classloader:/spec/one_spec.rb'
-    expect(Jars.home).to eq 'uri:classloader://jars'
+    expect(Dir.pwd).to eq 'uri:classloader://WEB-INF/classes/'
+    expect(__FILE__).to eq 'uri:classloader:/WEB-INF/classes/spec/one_spec.rb'
+    expect(Jars.home).to eq 'uri:classloader://WEB-INF/classes/jars'
     expect(Gem.dir).to eq 'uri:classloader://META-INF/jruby.home/lib/ruby/gems/shared'
   end
   
