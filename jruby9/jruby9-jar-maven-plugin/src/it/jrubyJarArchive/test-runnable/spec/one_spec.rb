9b552c415f219b85d6495a0a01d51d5d5b6c4ceb
Christian Meier <meier.kristian@gmail.com>
2015-11-02 11:34:24 +0100

    fix test on nested jars with gems

--- jruby9/jruby9-jar-maven-plugin/src/it/jrubyJarArchive/test-runnable/spec/one_spec.rb
+++ jruby9/jruby9-jar-maven-plugin/src/it/jrubyJarArchive/test-runnable/spec/one_spec.rb
@@ -4,7 +4,7 @@ describe 'something' do
   it 'runs inside the right environment' do
     expect(Dir.pwd).to eq 'uri:classloader://'
     expect(__FILE__).to eq 'uri:classloader:/spec/one_spec.rb'
-    expect(Jars.home).to eq 'uri:classloader://jars'
+    expect(Jars.home.sub('//', '/')).to eq 'uri:classloader:/jars'
     expect(Gem.dir).to eq 'uri:classloader://META-INF/jruby.home/lib/ruby/gems/shared'
   end
   
